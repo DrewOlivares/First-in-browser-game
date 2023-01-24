7a4badfbc248e92573f31fafaaea2632c757c23f
Andrew Olivares <Wrestler533438@gmail.com>
2023-01-24 13:30:24 -0800

    Added animation through css

--- code/index.js
+++
@@ -1,9 +0,0 @@
-function newImage(url){
-    let image = document.createElement('img')
-    image.src = url
-    image.style.position = 'absolute'
-    document.body.div.append(image)
-    return image
-}
-
-newImage(..\Pics\flying-enemy.png)
